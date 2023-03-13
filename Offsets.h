1477af609b16eb5a831a212b45f85a3abf3658d4
MonkeyStrongTogether <127679635+MonkeyStrongTogether@users.noreply.github.com>
2023-03-13 10:08:21 +0100

    Update Offsets.h

--- Offsets.h
+++ Offsets.h
@@ -6,7 +6,7 @@
 const size_t GAME_OPTIONS       = 0x020A77B4;
 const size_t WORLD_SNO          = 0x02261478;
 const size_t SELECTED_ACT       = 0x022A5DF8; //-1: Sanctuary | 0: A1 | 100: A2 | 200: A3 | 300: A4 | 400: A5 | Last selected when opening the World map
-const size_t VERSION_NUMBER     = 0x02329498;
+const size_t VERSION_NUMBER     = 0x02329498; //This is actually in a struct having stuff like ChallengeRiftNumber which you can change to get the `newest` CR build or older Challenge Rifts
 const size_t SELECTED_REGION    = 0x0232A804;
 const size_t MEMORY_MANAGER     = 0x0243D238;
 //Pointer
