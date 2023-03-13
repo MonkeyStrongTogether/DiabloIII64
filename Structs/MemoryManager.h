bdbb2141a98623eef961b192293ad945f62a5175
MonkeyStrongTogether <127679635+MonkeyStrongTogether@users.noreply.github.com>
2023-03-13 02:08:49 +0100

    .

--- Structs/MemoryManager.h
+++ Structs/MemoryManager.h
@@ -5,12 +5,12 @@ struct cMemoryManager
 {
 public:
     char            pad_0000[136];          //0x0000
-    cLocalHeap* LocalHeap;              //0x0088
+    cLocalHeap*     LocalHeap;              //0x0088
     char            pad_0090[8];            //0x0090
-    cLocalHeap* LocalHeap2;             //0x0098
-    cHeapNode* FirstNode;              //0x00A0
+    cLocalHeap*     LocalHeap2;             //0x0098
+    cHeapNode*      FirstNode;              //0x00A0
     char            pad_00A8[32];           //0x00A8
-    cDebugFile* DebugFile;              //0x00C8
+    cDebugFile*     DebugFile;              //0x00C8
     char            pad_00D0[72];           //0x00D0
     char            DiabloDirectory[64];    //0x0118
     char            pad_0158[2784];         //0x0158
